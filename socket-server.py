4c914f1e339cbb4da857b252b247714d4e408cea
Walter Werner <wernwa@gmail.com>
2014-11-08 09:13:16 -0800

    socket-server with curr time threading: TODO matching not working

--- socket-server.py
+++ socket-server.py
@@ -3,6 +3,59 @@
 import SocketServer
 import re
 from random import random
+import thread
+
+ps1=[random()*6, random()*30]
+ps2=[random()*6, random()*30]
+ps3=[random()*6, random()*30]
+ps4=[random()*6, random()*30]
+ps5=[random()*6, random()*30]
+ps6=[random()*6, random()*30]
+ps7=[random()*6, random()*30]
+ps8=[random()*6, random()*30]
+ps9=[random()*6, random()*30]
+ps_relee=[random()*6, random()*30]
+
+ps_VOLT=1
+ps_CURR=0
+
+nr_to_ps={
+    1:ps1,
+    2:ps2,
+    3:ps3,
+    4:ps4,
+    5:ps5,
+    6:ps6,
+    7:ps7,
+    8:ps8,
+    9:ps9,
+    31:ps_relee,
+        }
+
+ps_selected=None
+
+#INST:NSEL 3
+#:measure:voltage?
+#:measure:current?
+
+#INST:NSEL 6
+#TRIG:SOUR BUS
+#CURR:MODE WAVE
+#LIST:CURR 0
+#LIST:DWEL 4.52
+#LIST:COUNT 1
+#LIST:STEP AUTO
+#INIT:CONT OFF
+#INIT
+
+def frange(x, y, jump):
+    while x < y:
+        yield x
+        x += jump
+
+#regex_curr_change = re.compile(r'LIST:CURR (\S+)', re.MULTILINE)
+regex_curr_change = re.compile(r'INST:NSEL (\S+).*?LIST:CURR(\S+).*?LIST:DWEL (\S+)', re.DOTALL)
+
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
     """
@@ -13,19 +66,49 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
     client.
     """
 
+    def set_curr_in_time(self,ps, curr_1, time):
+        steps = 10.0
+        sleep_sec = time/steps
+        curr_0 = ps[ps_CURR]
+        curr_step = (curr_1 - curr_0)/steps
+
+        for i in frange(curr_0,curr_1,curr_step):
+            time.sleep(sleep_sec)
+            ps[ps_CURR]=i
+        time.sleep(sleep_sec)
+        ps[ps_CURR]=curr_1
+
+
+
     def handle(self):
         while True:
             # self.request is the TCP socket connected to the client
             self.data = self.request.recv(1024).strip()
             if not self.data: break
-            print "----- {} wrote: -----".format(self.client_address[0])
-            print self.data
+            #print "----- {} wrote: -----".format(self.client_address[0])
+            #print self.data
 
-            m = re.search('\?',self.data)
+            m = re.search('INST:NSEL (\d+)',self.data)
             if m!=None:
-                num = "%.3f\r\n" %(random()*4)
-                # send back the random number
+                ps_selected=nr_to_ps[int(m.group(1))]
+
+            if ':measure:voltage?' in self.data:
+                num = "%.3f\r\n" %(ps_selected[ps_VOLT])
                 self.request.sendall(num)
+
+            elif ':measure:current?' in self.data:
+                num = "%.3f\r\n" %(ps_selected[ps_CURR])
+                self.request.sendall(num)
+            elif '*IDN?' in self.data:
+                num = "IDN=%.3f\r\n" %(random())
+                self.request.sendall(num)
+
+            matches = [m.groups() for m in regex_curr_change.finditer(self.data)]
+            for m in matches:
+                #ps_selected=nr_to_ps[int(m[0])]
+                #ps_selected[ps_CURR]=float(m[1])
+                #curr_time = float(m[2]) # for later use
+                thread.start_new_thread(this.set_curr_in_time,(int(m[0]),float(m[1]),float(m[2]),))
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8003
